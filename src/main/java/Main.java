f60c68f534021ec14af1484adcf2e76ac258f210
acoliztac <acoliztac@gmail.com>
2016-08-19 21:51:36 +0300

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import static java.lang.Character.isDigit;
@@ -8,21 +11,20 @@ import static java.lang.Math.sqrt;
  * Created by Никита on 18.08.2016.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("\tИнвертированная польская запись.\n" +
                 "Данный алгоритм предназначен для вычисления формул, содержащих арифметические операции:" +
                 "\nСложение. Вычитание. Умножение. Деление.\n" +
-                "Возведение в степень. Извлечение корня.  ");
+                "Возведение в степень. Извлечение корня.  " +
+                "\n\n\tВведите уравнение для вычисления:"
+        );
 
-        String s = "2 + 1";                         //3.0
-        String s1 = "(2 / 3 + 1)";                  //1.(6)
-        String s2 = "4 * 5+3";                      //23.0
-        String s3 = "1 - 2*3 + 4";                  //-1.0
-        String s4 = "1 + 2 * (3 - 4)";              //-1.0
-        String s5 = "5 * 3 - 4 ^ 2 +2/(2 -11)";     //1.(2)
-        String s6 = "2 * (3-1) - (2 + 1) + 12";     //13.0
-//        solution(s6);
-        System.out.println(sqrt(9));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+//        String expression = reader.readLine();
+        reader.close();
+        String expression = "2 + 3";
+        solution(expression);
     }
 
 
@@ -36,7 +38,7 @@ public class Main {
                 i -= 2;
             }
         }
-        System.out.println(polish.get(0));
+        System.out.println("\tРезультат = " + polish.get(0));
     }
 
     private static void decision(ArrayList<String> polish, int i) {
@@ -59,14 +61,14 @@ public class Main {
 
     private static ArrayList<String> converterToPolish(String text) {
         text = text.replaceAll(" ", "");
-//        System.out.println(text + " - содержимое входной строки");
+        System.out.println(text + ": форматированное исходное выражение");
 
         ArrayList<String> result = mainInitialization(text);
 
-//        for (String s : result){
-//            System.out.print(s + " ");
-//        }
-//        System.out.println(" - польская запись");
+        for (String s : result){
+            System.out.print(s + " ");
+        }
+        System.out.println(": форматированная польская запись");
         return result;
     }
 
