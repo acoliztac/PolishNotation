a73fbfda3c35ccabd2ba7f55710fa26d345c90ad
acoliztac <acoliztac@gmail.com>
2016-08-19 21:35:02 +0300

    Workable solution without root extraction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,73 @@
 import java.util.ArrayList;
 
 import static java.lang.Character.isDigit;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
 /**
  * Created by Никита on 18.08.2016.
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\tИнвертированная польская запись.\n" +
+                "Данный алгоритм предназначен для вычисления формул, содержащих арифметические операции:" +
+                "\nСложение. Вычитание. Умножение. Деление.\n" +
+                "Возведение в степень. Извлечение корня.  ");
 
-        String s = "2 + 1";
-        String s1 = "(2 / 3 + 1)";
-        String s2 = "4 * 5+3";
-        String s3 = "1 - 2*3 + 4";
-        String s4 = "1 + 2 * (3 - 4)";
-        String s5 = "5 * 3 - 4 ^ 2 +2/(2 -11)";
-        String s6 = "2 * (3-1) - (2 + 1) + 12";
-        converterToPolish(s);
-        
+        String s = "2 + 1";                         //3.0
+        String s1 = "(2 / 3 + 1)";                  //1.(6)
+        String s2 = "4 * 5+3";                      //23.0
+        String s3 = "1 - 2*3 + 4";                  //-1.0
+        String s4 = "1 + 2 * (3 - 4)";              //-1.0
+        String s5 = "5 * 3 - 4 ^ 2 +2/(2 -11)";     //1.(2)
+        String s6 = "2 * (3-1) - (2 + 1) + 12";     //13.0
+//        solution(s6);
+        System.out.println(sqrt(9));
     }
 
-    private static void converterToPolish(String text) {
+
+    private static void solution(String s) {
+        ArrayList<String> polish = converterToPolish(s);
+        for (int i = 0; i < polish.size(); i++) {
+            try {
+                double tmp = Double.parseDouble(polish.get(i));
+            } catch (NumberFormatException e) {
+                decision(polish, i);
+                i -= 2;
+            }
+        }
+        System.out.println(polish.get(0));
+    }
+
+    private static void decision(ArrayList<String> polish, int i) {
+        double first = Double.parseDouble(polish.get(i - 2));
+        double second = Double.parseDouble(polish.get(i - 1));
+        if (polish.get(i).equals("+")){
+            polish.set(i - 2, String.valueOf(first + second));
+        } else if (polish.get(i).equals("-")){
+            polish.set(i - 2, String.valueOf(first - second));
+        } else if (polish.get(i).equals("*")){
+            polish.set(i - 2, String.valueOf(first * second));
+        } else if (polish.get(i).equals("/")){
+            polish.set(i - 2, String.valueOf(first / second));
+        } else if (polish.get(i).equals("^")){
+            polish.set(i - 2, String.valueOf(pow(first, second)));
+        }
+        polish.remove(i);
+        polish.remove(i - 1);
+    }
+
+    private static ArrayList<String> converterToPolish(String text) {
         text = text.replaceAll(" ", "");
-        System.out.println(text + " - содержимое входной строки");
+//        System.out.println(text + " - содержимое входной строки");
 
         ArrayList<String> result = mainInitialization(text);
 
-        for (String s : result){
-            System.out.print(s + " ");
-        }
-        System.out.println(" - результат");
+//        for (String s : result){
+//            System.out.print(s + " ");
+//        }
+//        System.out.println(" - польская запись");
+        return result;
     }
 
     private static ArrayList<String> mainInitialization(String text) {
